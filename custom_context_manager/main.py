f17bfc7ac2b8a810c59ed34f764c946b2bc51cc1
Subhranil Sarkar <subhranil2605@gmail.com>
2024-01-30 13:16:47 +0530

    Add context manager with decorator

--- custom_context_manager/main.py
+++ custom_context_manager/main.py
@@ -1,25 +1,26 @@
 import logging
 import sqlite3
+from contextlib import contextmanager
 
 
-class SQLite:
-    def __init__(self, file_name: str):
-        self.file_name = file_name
-        self.connection = sqlite3.connect(self.file_name)
-
-    def __enter__(self):
-        logging.info("Calling __enter__")
-        return self.connection.cursor()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        logging.info("Calling __exit__")
-        self.connection.commit()
-        self.connection.close()
+@contextmanager
+def open_db(file_name: str):
+    connection = sqlite3.connect(file_name)
+    try:
+        logging.info("Creating connection")
+        cursor = connection.cursor()
+        yield cursor
+    except sqlite3.DatabaseError as error:
+        logging.error(error)
+    finally:
+        logging.info("Closing connection")
+        connection.commit()
+        connection.close()
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    with SQLite(file_name="application.db") as cursor:
+    with open_db(file_name="application.db") as cursor:
         cursor.execute("SELECT * FROM blogs")
         logging.info(cursor.fetchall())
 
